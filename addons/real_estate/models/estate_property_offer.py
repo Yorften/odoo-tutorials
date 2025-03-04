6ad48d6435ff507df57a5a875da4ceef26235bb9
Yorften <abdr7mane.bac@gmail.com>
2025-03-04 10:03:30 +0000

    added valcidity and date deadline to estate offers

--- addons/real_estate/models/estate_property_offer.py
+++ addons/real_estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.tools.date_utils import relativedelta
 
 
 class EstatePropertyOffer(models.Model):
@@ -13,7 +14,33 @@ class EstatePropertyOffer(models.Model):
         ],
         copy=False,
     )
+    validity = fields.Integer(string="Validity (days)", default=7)
+    date_deadline = fields.Date(
+        string="Date Deadline",
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+        store=True,
+    )
 
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
     property_type_id = fields.Many2one(related="property_id.property_type_id")
+
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for offer in self:
+            base_date = (
+                offer.create_date.date() if offer.create_date else fields.Date.today()
+            )
+            offer.date_deadline = base_date + relativedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            base_date = (
+                offer.create_date.date() if offer.create_date else fields.Date.today()
+            )
+            if offer.date_deadline:
+                difference = (offer.date_deadline - base_date).days
+                offer.validity = max(difference, 0)
+            else:
+                offer.validity = 0
