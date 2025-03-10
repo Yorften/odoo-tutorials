8d8c5d172ee91342dff2859858007c8c04caff59
Yorften <abdr7mane.bac@gmail.com>
2025-03-10 11:24:05 +0000

    added properties page to users view

--- addons/real_estate/__manifest__.py
+++ addons/real_estate/__manifest__.py
@@ -22,6 +22,7 @@
         "views/estate_property_type_views.xml",
         "views/estate_property_offer_views.xml",
         "views/estate_property_tag_views.xml",
+        "views/user_views.xml",
         "views/estate_menu.xml",
     ],
     "assets": {
--- addons/real_estate/models/res_users.py
+++ addons/real_estate/models/res_users.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api, _
 
 
-class EstateUsers(models.Model):
+class ResUsers(models.Model):
     _inherit = "res.users"
 
     property_ids = fields.One2many("estate.property", "salesperson_id", domain=[("state", "in", ("new", "recieved"))])
