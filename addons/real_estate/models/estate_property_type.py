4fa0288f077e5b5c989fc28ab2551828747f2846
Yorften <abdr7mane.bac@gmail.com>
2025-03-10 09:20:11 +0000

    added default ordering

--- addons/real_estate/models/estate_property_type.py
+++ addons/real_estate/models/estate_property_type.py
@@ -8,6 +8,7 @@ class EstatePropertyType(models.Model):
 
     name = fields.Char(string="Name", required=True)
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
+    sequence = fields.Integer("Sequence", default=1, help="Used to order types. Lower is better.")
 
     _sql_constraints = [
         (
